5dd8e8138a245c65590f2659eed46ed6e0319755
Praveen Thirumurugan <mail@praveent.com>
2025-12-02 17:23:22 +0530

    Update first launch detection

--- Illustrate/IllustrateApp.swift
+++ Illustrate/IllustrateApp.swift
@@ -1,11 +1,35 @@
 import SwiftData
 import SwiftUI
+import Combine
+
+final class AppVersionManager: ObservableObject {
+    @Published var isTheFirstLaunch: Bool = true
+
+    init() {
+        checkFirstLaunch()
+    }
+
+    private func checkFirstLaunch() {
+        let hasLaunchedBefore = UserDefaults.standard.bool(forKey: "hasLaunchedBefore")
+        isTheFirstLaunch = !hasLaunchedBefore
+
+        if !hasLaunchedBefore {
+            UserDefaults.standard.set(true, forKey: "hasLaunchedBefore")
+        }
+    }
+}
 
 @main
 struct IllustrateApp: App {
+    @StateObject private var appVersionManager = AppVersionManager()
+    @StateObject private var connectionService = ConnectionService.shared
+
     var body: some Scene {
         WindowGroup {
             MainView()
+                .environmentObject(appVersionManager)
+                .environmentObject(connectionService)
+                .environment(\.connectionService, connectionService)
                 .modelContainer(
                     for: [
                         Connection.self,
